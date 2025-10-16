7c640795f99f8a70e8b3d2dc0dd0d26425ed48bf
beamiter <beamiter@163.com>
2025-10-16 15:44:03 +0800

    fix lib

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -7,7 +7,7 @@
 //! let code = r#"
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_vim::LANGUAGE;
+//! let language = tree_sitter_vim9::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
 //!     .expect("Error loading Vim parser");
@@ -21,11 +21,11 @@
 use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_vim() -> *const ();
+    fn tree_sitter_vim9() -> *const ();
 }
 
 /// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_vim) };
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_vim9) };
 
 /// The content of the [`node-types.json`] file for this grammar.
 ///
